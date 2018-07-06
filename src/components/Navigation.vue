692c56461832df1dda593a39770d0eb0f40f15d3
Dennis Kleber <dennis_kleber@mailbox.org>
2018-07-06 14:42:56 +0200

    first navigation widget version

---
+++ src/components/Navigation.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="navbar navbar-vca navbar-default navbar-fixed-top"> <!-- navbar-default -->
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="@routes.Application.index">
+						<img src="images/drop_small.png"/>
+            <div>
+              <span class="bold">Pool²</span>
+              <span>Vica con Agua</span>
+            </div>
+          </a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="sr-only">navbar.toogle.nav</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+			<div class="navbar-collapse collapse" id="navbar-main">
+	<ul class="nav navbar-nav navbar-right">
+		<li v-for="entry in entrys">
+			<a v-bind:href="entry.entryURL">{{ entry.entryLable }}</a>
+		</li>
+	</ul>
+ </div>
+      </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data () {
+    return {
+      entrys: [
+        {
+          entryURL: 'test',
+					entryLable: 'test'
+        }
+      ]
+    }
+  }
+}
+</script>
